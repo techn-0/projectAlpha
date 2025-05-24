7e6d3eb1e9af9764a0d891e305172717b0c25649
techn-0 <gnlrjswjd@naver.com>
2025-05-24 16:29:16 +0900

    이동 시 타일 생성

---
+++ Assets/Undead Survivor/Codes/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // 싱글톤 패턴, 스테틱은 인스펙터 표시X
+    public Player player;
+
+    void Awake()
+    {
+        instance = this; // 싱글톤 패턴
+    }
+}
