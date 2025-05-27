b93ff682cbd06c66322609703ddf0f877efbccde
techn-0 <gnlrjswjd@naver.com>
2025-05-27 22:42:50 +0900

    무기 갯수별 각도 설정

--- Assets/Undead Survivor/Codes/Weapon.cs
+++ Assets/Undead Survivor/Codes/Weapon.cs
@@ -45,6 +45,12 @@ public class Weapon : MonoBehaviour
         {
             Transform bullet = GameManager.instance.pool.GetObject(prefabId).transform; // 풀에서 오브젝트 가져오기
             bullet.parent = transform; // 현재 오브젝트의 자식으로 설정
+
+            Vector3 rotVec = Vector3.forward * 360f * i / count; 
+            bullet.Rotate(rotVec); // 회전 설정
+            bullet.Translate(bullet.up * 1.5f, Space.World); // 위치 설정
+
+
             bullet.GetComponent<Bullet>().Init(damage, -1); // Bullet 컴포넌트 초기화, -1은 무한관통(per), 근접공격
         }
     }
