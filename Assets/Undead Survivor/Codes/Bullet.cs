01ce6cae1beb190052348043493e9874d97693ea
techn-0 <gnlrjswjd@naver.com>
2025-05-27 22:00:09 +0900

    무기 생성

---
+++ Assets/Undead Survivor/Codes/Bullet.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damage;
+    public int per;
+    public void Init(float damage, int per)
+    {
+        this.damage = damage;
+        this.per = per;
+    }
+}
