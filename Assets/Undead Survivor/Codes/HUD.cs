dd8ddc78713b1e71c11a28fbc9e2fd9388973c23
techn-0 <gnlrjswjd@naver.com>
2025-05-30 21:06:27 +0900

    킬수 레벨 경험치 시간 UI

--- Assets/Undead Survivor/Codes/HUD.cs
+++ Assets/Undead Survivor/Codes/HUD.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum InfoType { Exp, Level, Kill, Time, Health }
+    public InfoType type;
+
+    Text myText;
+    Slider mySlider;
+
+    void Awake()
     {
-        
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        switch (type)
+        {
+            case InfoType.Exp:
+                float curExo = GameManager.instance.exp;
+                float maxExo = GameManager.instance.nextExp[GameManager.instance.level];
+                mySlider.value = curExo / maxExo;
+                break;
+            case InfoType.Level:
+            myText.text = "Lv." + GameManager.instance.level.ToString();
+                break;
+            case InfoType.Kill:
+                myText.text = GameManager.instance.kill.ToString();
+                break;
+            case InfoType.Time:
+                float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                int min = Mathf.FloorToInt(remainTime / 60);
+                int sec = Mathf.FloorToInt(remainTime % 60);
+                myText.text = string.Format("{0:D2}:{1:D2}", min, sec);
+                break;
+            case InfoType.Health:
+                break;
+        }
     }
 }
